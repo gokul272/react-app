25cacb8e337558a8eed14a901e34313b18d8e194
Arun2325 <karunkumar7325@gmail.com>
2021-03-25 16:39:08 +0530

    Updates

---
+++ precache-manifest.d871a6482268053ac57288bb152ef65f.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d643cec1a778fe52ded404877588f07",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "7b639f3693b22e4b930c",
+    "url": "/react-app/static/css/2.28cec220.chunk.css"
+  },
+  {
+    "revision": "2c67f9623f8b5a7e5fbc",
+    "url": "/react-app/static/css/main.cf7d7913.chunk.css"
+  },
+  {
+    "revision": "7b639f3693b22e4b930c",
+    "url": "/react-app/static/js/2.63917158.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/react-app/static/js/2.63917158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c67f9623f8b5a7e5fbc",
+    "url": "/react-app/static/js/main.6266155a.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  },
+  {
+    "revision": "35f4a9247edd8add2789c41c1d02a77d",
+    "url": "/react-app/static/media/Hosur-City.35f4a924.jpg"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "/react-app/static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "c94f190610f0f6a9fe0a40c2251727ce",
+    "url": "/react-app/static/media/ac1.c94f1906.jpg"
+  },
+  {
+    "revision": "4fbc2941493563e146250ae74e51d199",
+    "url": "/react-app/static/media/ac2.4fbc2941.jpg"
+  },
+  {
+    "revision": "258a7916172e37e98e8f5529ccc499e7",
+    "url": "/react-app/static/media/banumathi.258a7916.jpg"
+  },
+  {
+    "revision": "20b7c945054e6944e252ad6210954db6",
+    "url": "/react-app/static/media/bsnl-logo.20b7c945.png"
+  },
+  {
+    "revision": "5cd06b554ab3664698de204be991b407",
+    "url": "/react-app/static/media/capgemini.5cd06b55.png"
+  },
+  {
+    "revision": "203636d138667d1cc3153dc03713ea99",
+    "url": "/react-app/static/media/crmit.203636d1.png"
+  },
+  {
+    "revision": "e05a995b6c3c2cfb27ca5a75d4400271",
+    "url": "/react-app/static/media/dean.e05a995b.jpg"
+  },
+  {
+    "revision": "cc4ce17a46bd25a88767ee6681f05c06",
+    "url": "/react-app/static/media/google.cc4ce17a.png"
+  },
+  {
+    "revision": "0867f431ae13bd0ee63f5c4a6c0f660a",
+    "url": "/react-app/static/media/infosys-logo.0867f431.png"
+  },
+  {
+    "revision": "83eab36f8ebe21cd09608ea73d95523a",
+    "url": "/react-app/static/media/microsoft-logo.83eab36f.png"
+  },
+  {
+    "revision": "c7ee3db5d862473956b91aa971f959cb",
+    "url": "/react-app/static/media/msdn-logo.c7ee3db5.png"
+  },
+  {
+    "revision": "8cf42c48e6f7de87869171576807cc67",
+    "url": "/react-app/static/media/principal.8cf42c48.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "10a7465c160bec8226fcd8bc50cc9447",
+    "url": "/react-app/static/media/slider1.10a7465c.jpg"
+  },
+  {
+    "revision": "0d42de30fbf3e66af8570537b6d76dcb",
+    "url": "/react-app/static/media/slider2.0d42de30.jpg"
+  },
+  {
+    "revision": "7d044aa7057e0d08ca077429ef55a6ca",
+    "url": "/react-app/static/media/slider3.7d044aa7.jpg"
+  },
+  {
+    "revision": "357798af9be3deb352f2912dbd96f1cf",
+    "url": "/react-app/static/media/tcs.357798af.png"
+  },
+  {
+    "revision": "00e17c4a4416b229b0633aaf64937aea",
+    "url": "/react-app/static/media/userpic.00e17c4a.png"
+  },
+  {
+    "revision": "0a03096b7e7aa1bcc20a71964243cced",
+    "url": "/react-app/static/media/wipro-logo.0a03096b.png"
+  },
+  {
+    "revision": "3f5cc83f748b987bb4a0279ff6e98b53",
+    "url": "/react-app/static/media/zifo.3f5cc83f.png"
+  }
+]);
