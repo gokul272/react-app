638a00cf31c89e2eb26fa47755b951e74b591af5
gokul272 <82793773+gokul272@users.noreply.github.com>
2021-04-23 11:14:25 +0530

    Update Footer.js

--- src/components/Footer.js/Footer.js
+++ src/components/Footer.js/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
-          <img className="logo1" src="../../images/aoclogo1.png" alt="logo"/>
+          <img className="logo1" src="../images/aoclogo1.png" alt="logo"/>
           </div>
           <div className='footer-link-items'>
             <h2>About Us</h2>
